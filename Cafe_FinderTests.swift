429c2b66cc104b5be9dece67284df411ba34d05c
Reyna Dai Luo <rdailuo77@gmail.com>
2025-10-06 13:45:46 -0400

    Add files via upload

---
+++ Cafe_FinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cafe_FinderTests.swift
+//  Cafe FinderTests
+//
+//  Created by Kamen Dai on 10/6/25.
+//
+
+import Testing
+@testable import Cafe_Finder
+
+struct Cafe_FinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
